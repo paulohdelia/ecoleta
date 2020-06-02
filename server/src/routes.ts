891ed7dba349998cfb89b8fb5a29e2468ed28e06
paulodelia <paulohdelia@gmail.com>
2020-06-02 15:04:00 -0300

    fix: add trx.commit() to PointsController create - server

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,8 +7,14 @@ const routes = express.Router();
 const itemsController = new ItemsController();
 const pointsController = new PointsController();
 
+/* Items */
 routes.get('/items', itemsController.index);
 
+/* Points */
 routes.post('/points', pointsController.create);
+
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
 
 export default routes;
