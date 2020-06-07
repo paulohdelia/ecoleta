6a306a138c61be9972e5646d0eb9aa8c847c4a7f
paulodelia <paulohdelia@gmail.com>
2020-06-07 17:36:39 -0300

    feat: data validation with celebrate - server

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import path from 'path';
 
 import routes from './routes';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -12,5 +13,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333);
